63a41abe4396b071f3e4b3fdccf3e30621b0df96
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-06 11:39:11 -0500

    Add files via upload

--- PolynomialEvaluator.java
+++ PolynomialEvaluator.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
-
 import java.util.StringTokenizer;
 
 public class PolynomialEvaluator {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
+        String function = "f(x) = ";
+        double sum = 0.0;
 
         System.out.println("Please enter a polynomial in the form of f(x) = 3x^3 -5x^2 +2:");
-        String polynomial = keyboard.next();
+        String polynomial = keyboard.nextLine();
         System.out.println("Please enter a value for x:");
         double x = keyboard.nextDouble();
         keyboard.close();
 
-        StringTokenizer token = new StringTokenizer(polynomial, " ");
+        StringTokenizer polySt = new StringTokenizer(polynomial);
+        int term = polynomial.indexOf('x');
+        // int hat = indexOf('^');
 
-        while (token.hasMoreTokens()) {
-            String temp = token.nextToken();
-            for (int i = 0; i < temp.length(); i++) {
-                if (Character.isDigit(temp.charAt(i))) {
-                    System.out.println(temp.charAt(i));
-                }
+        while (polySt.hasMoreTokens()) {
+            System.out.println(term);
+
+            // double coefficient = Double.parseDouble(coeffSt.nextToken());
+            // int exponent = Integer.parseInt(expSt.nextToken());
+
+            // function += coefficient + "x^" + exponent + " ";
+            // sum += coefficient * Math.pow(x, exponent);
             }
         }
+        // System.out.println("Your polynomial function is:" + function);
+        // System.out.println("Your polynomial at x = " + x + " is: f(" + x + ") = " + sum);
     }
-}
