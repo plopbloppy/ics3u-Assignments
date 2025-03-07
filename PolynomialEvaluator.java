9d7db9a5a0b1d68e9a7ab62aad8de29fdee9fce5
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-06 22:49:54 -0500

    Add files via upload

--- PolynomialEvaluator.java
+++ PolynomialEvaluator.java
@@ -6,27 +6,29 @@ public class PolynomialEvaluator {
         Scanner keyboard = new Scanner(System.in);
         String function = "f(x) = ";
         double sum = 0.0;
+        double constant = 0.0;
 
         System.out.println("Please enter a polynomial in the form of f(x) = 3x^3 -5x^2 +2:");
-        String polynomial = keyboard.nextLine();
+        String userInput = keyboard.nextLine();
+        String polynomial = userInput.substring(userInput.indexOf('=') + 1);
+
         System.out.println("Please enter a value for x:");
-        double x = keyboard.nextDouble();
+        int x = keyboard.nextInt();
         keyboard.close();
 
         StringTokenizer polySt = new StringTokenizer(polynomial);
-        int term = polynomial.indexOf('x');
-        // int hat = indexOf('^');
 
         while (polySt.hasMoreTokens()) {
-            System.out.println(term);
+            String term = polySt.nextToken();
+            int variable = term.indexOf('x');
+            int hat = term.indexOf('^');
+            double coef = Double.parseDouble(term.substring(0, variable + 1));
+            int exp = Integer.parseInt(term.substring(hat + 1));
 
-            // double coefficient = Double.parseDouble(coeffSt.nextToken());
-            // int exponent = Integer.parseInt(expSt.nextToken());
-
-            // function += coefficient + "x^" + exponent + " ";
-            // sum += coefficient * Math.pow(x, exponent);
+            function += coef + "x^" + exp + " ";
+            sum += coef * Math.pow(x, exp);
             }
+        System.out.println("Your polynomial function is:" + function);
+        System.out.println("Your polynomial at x = " + x + " is: f(" + x + ") = " + sum);
         }
-        // System.out.println("Your polynomial function is:" + function);
-        // System.out.println("Your polynomial at x = " + x + " is: f(" + x + ") = " + sum);
     }
