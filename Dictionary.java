18a4a54da8371b7d6d9496d14faa6a49f03b2e87
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-27 11:55:32 -0400

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,5 +1,16 @@
-public class App {
+import java.util.Scanner;
+
+public class Dictionary {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Please enter your sentence");
+        String input = keyboard.nextLine();
+
+        int period = input.indexOf(".");
+        String[] words = (input.indexOf(0, period)).trim(" ");
+
+        for (int i = 0; i < words.length; i++) {
+        }
     }
 }
