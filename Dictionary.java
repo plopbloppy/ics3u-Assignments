0c94e7d49e05a3c82e291b3dad760b4613a07fee
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-28 11:14:46 -0400

    Add files via upload

--- Dictionary.java
+++ Dictionary.java
@@ -1,16 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Dictionary {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
-        
-        System.out.println("Please enter your sentence");
-        String input = keyboard.nextLine();
+        HashSet<String> dictionary = new HashSet<String>();
+        BufferedReader inputStream = null;
+        String line = null;
 
-        int period = input.indexOf(".");
-        String[] words = (input.indexOf(0, period)).trim(" ");
-
-        for (int i = 0; i < words.length; i++) {
+        try {
+            inputStream = new BufferedReader(new FileReader("dictionary.txt"));
+            while ((line = inputStream.readLine()) != null) {
+                dictionary.add(line);
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("Error opening file!");
+            System.exit(1);
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
         }
+
+        System.out.println("Please enter your sentence:");
+        String input = keyboard.nextLine().trim();
+
+        String word = input.contains(".") ? input.substring(0, input.indexOf(".")) : input;
+
+        String[] words = word.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            String isValid = dictionary.contains(words[i].toLowerCase()) ? "<valid>" : "<invalid>";
+            System.out.println((i + 1) + "." + words[i] + " " + isValid);
+        }
+        keyboard.close();
     }
 }
