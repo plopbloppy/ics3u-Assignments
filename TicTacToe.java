6298486d10a6ecfdfb140cd188d72bd0188b2676
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-30 00:13:06 -0400

    Add files via upload

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Scanner;
 
 public class TicTacToe {
     public static void board(String[][] board) {
@@ -18,39 +18,59 @@ public class TicTacToe {
         int row = Integer.parseInt(input.substring(1));
 
         switch (col) {
-            case "A":
-                board[row + 1][1] = ("|" + turn);
-                break;
-            case "B":
-                board[row + 1][2] = ("|" + turn);
-                break;
-            case "C":
-                board[row + 1][3] = ("|" + turn + "|");
-                break; 
+            case "A" -> board[row + 1][1] = ("|" + turn);
+            case "B" -> board[row + 1][2] = ("|" + turn);
+            case "C" -> board[row + 1][3] = ("|" + turn + "|");
         }
     }
 
-    public static void checkForWin(int i, String[][] board, String turn, String input) {
-        String col = input.substring(0, 1);
-        int winCounter = 0;
+    public static boolean checkForWin(String[][] board, String turn, String input) {
+        String letterCol = input.substring(0, 1);
+        int rowInput = Integer.parseInt(input.substring(1));
+        int col = 0;
+        int row = 0;
+        int verticalWinCounter = 0;
+        int horizontalWinCounter = 0;
+        boolean diagonalWin = false;    
 
-        switch (col) {
-            case "A":
-                for (i = 2; i < 5; i++) {
-                    winCounter = board[i][1].contains(turn) ? winCounter++ : 0;
-                }
-                if (winCounter == 3) {
-                    System.out.println("'" + turn + "' has won!!!");
-                }
+        switch (letterCol) {
+            case "A" -> col = 1;
+            case "B" -> col = 2;
+            case "C" -> col = 3;
         }
+
+        switch (rowInput) {
+            case 1 -> row = 2;
+            case 2 -> row = 3;
+            case 3 -> row = 4;
+        }
+
+        for (int i = 2; i < 5; i++) {
+            if (board[i][col].contains(turn)) {
+                verticalWinCounter++;
+            }
+        }
+
+        for (int i = 1; i < 4; i++) {
+            if (board[row][i].contains(turn)) {
+                horizontalWinCounter++;
+            }
+        }
+
+        if (board[2][1].contains(turn) && board[3][2].contains(turn) && board[4][3].contains(turn) || 
+            board[4][1].contains(turn) && board[3][2].contains(turn) && board[2][3].contains(turn)) {
+            diagonalWin = true;
+        }
+        boolean win = verticalWinCounter == 3 || horizontalWinCounter == 3 || diagonalWin;
+        return win;
     }
 
     public static void main(String[] args) throws Exception {
-        Scanner keyboard = new Scanner(System.in);
         HashSet<String> playsMade = new HashSet<String>();
+        Scanner keyboard = new Scanner(System.in);
+        final int MAX_TOLERANCE = 8;
         int i = 0;
-        String turn = i % 2 == 0 ? "o" : "x";
-        String input = "";        
+        String input = "";
 
         String[][] board = { { "   ", " A ", "B ", "C" },
                 { "   ", " _ ", "_ ", "_" },
@@ -62,42 +82,43 @@ public class TicTacToe {
         System.out.println(
                 "In this game, your objective is to have three consequence pieces diagonally or in a row/column.");
         System.out.println("'o' plays first, and 'x' plays second.");
-        System.out.println("To place your piece, enter the column and the row, in the form of 'A1'.");
+        System.out.println("To place your piece, enter the column and the row, in the form of 'A1' (case-sensitive).");
 
         while (true) {
+            String turn = i % 2 == 0 ? "o" : "x";
             board(board);
             System.out.println("Turn: " + turn);
-    
-            try {
-                System.out.println("Place your piece:");
-                input = keyboard.next();
 
-                Pattern pattern = Pattern.compile([A-C]);
-                Matcher matcher = pattern.matcher("Vdfasdfasd");
-                boolean matchFound = matcher.find();
+            System.out.println("Place your piece:");
+            input = keyboard.next();
 
-                if (input.matches(/[A-C])) {
-                    if (playsMade.contains(input)) {
-                        System.out.println("That move has already been entered!");
-                        i--;
-                    } else {
-                        placePiece(board, turn, input);
-                        checkForWin(i, board, turn, input);
-                    }
+            Pattern pattern = Pattern.compile("[ABC]+[1-3]");
+            Matcher matcher = pattern.matcher(input);
+            boolean matchFound = matcher.find();
+
+            if (matchFound) {
+                if (playsMade.contains(input)) {
+                    System.out.println("That move has already been entered!");
+                    i--;
                 } else {
-                    System.out.println("Please use the correct form! (e.g. C2)");
+                    placePiece(board, turn, input);
+                    if (checkForWin(board, turn, input)) {
+                        board(board);
+                        System.out.println("Player '" + turn + "' has won!!!");
+                        keyboard.close();
+                        System.exit(1);
+                    } else if (i == MAX_TOLERANCE) {
+                        board(board);
+                        System.out.println("Game is a tie!");
+                        keyboard.close();
+                        System.exit(1);
+                    }
                 }
-                     
-                System.out.println("-------------");
-            } catch (NumberFormatException e) {
+            } else {
                 System.out.println("Please use the correct form! (e.g. B3)");
-                System.out.println("-------------");
-                i--;
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Not an valid spot!");
-                System.out.println("-------------");
                 i--;
             }
+            System.out.println("-------------");
             playsMade.add(input);
             i++;
         }
