6cdd12db4f057896d6820c759d7150e06045366a
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-04-02 11:02:29 -0400

    Update Dictionary.java

--- Dictionary/Dictionary.java
+++ Dictionary/Dictionary.java
@@ -1,1 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Scanner;
 
+public class Dictionary {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        HashSet<String> dictionary = new HashSet<String>();
+        BufferedReader inputStream = null;
+        String line = null;
+
+        String[] punctuation = {".", "!", "?", "'", ";", "-", "(", ")"};
+
+        try {
+            inputStream = new BufferedReader(new FileReader("dictionary.txt"));
+            while ((line = inputStream.readLine()) != null) {
+                dictionary.add(line);
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("Error opening file!");
+            System.exit(1);
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        System.out.println("Please enter your sentence:");
+        String input = keyboard.nextLine().trim();
+        keyboard.close();
+        
+        for (int i = 0; i < punctuation.length; i++) {
+            if (input.contains(punctuation[i])) {
+                input.replaceAll(punctuation[i], "");
+            }
+        }
+
+        String[] words = input.split(" ");
+        
+        for (int i = 0; i < words.length; i++) {
+            String isValid = dictionary.contains(words[i].toLowerCase()) ? "<valid>" : "<invalid>";
+            System.out.println((i + 1) + "." + words[i] + " " + isValid);
+        }
+    }
+}
