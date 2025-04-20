0fc5dc80dea3b5de50db5b934e5b9fdc6853ed1e
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-04-20 12:48:41 -0400

    Create IonicCompoundMassCalculator.java

---
+++ IonicCompoundMassCalculator/IonicCompoundMassCalculator.java
@@ -0,0 +1,7 @@
+public class IonicCompoundMassCalculator {
+
+    public static void main(String[] args) {
+        Element.NITROGEN.printIonicCompoundMass(Element.LITHIUM);
+        Element.NITROGEN.printIonicCompound(Element.LITHIUM);
+    }
+}
