786c36fe39766a897c641461e3e73acd6876aaff
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-04-21 16:58:18 -0400

    Add files via upload

--- IonicCompoundMassCalculator/IonicCompoundMassCalculator.java
+++ IonicCompoundMassCalculator/IonicCompoundMassCalculator.java
@@ -1,7 +1,8 @@
 public class IonicCompoundMassCalculator {
 
     public static void main(String[] args) {
+        System.out.println(Element.BERYLLIUM.getAtomicMass());
         Element.NITROGEN.printIonicCompoundMass(Element.LITHIUM);
-        Element.NITROGEN.printIonicCompound(Element.LITHIUM);
+        Element.NITROGEN.printIonicCompound(Element.NITROGEN);
     }
 }
