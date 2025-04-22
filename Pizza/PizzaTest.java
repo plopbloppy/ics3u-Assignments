a26010b96ada2dad926d87c0e5d75e5a59df9b3b
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-04-22 11:38:22 -0400

    Create PizzaTest.java

---
+++ Pizza/PizzaTest.java
@@ -0,0 +1,9 @@
+public class PizzaTest {
+
+    public static void main(String[] args) {
+        Pizza pizza = new Pizza("CHEDDAR", "pesto", 2);
+
+        pizza.eatPizza();
+        pizza.tasteCheese();
+    }
+}
