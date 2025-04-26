f7785307e0c91791c5790470ce2a992f7e30c61b
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-04-25 21:40:57 -0400

    Create Coop.java

---
+++ FarmAnimal/Coop.java
@@ -0,0 +1,12 @@
+import FarmAnimalEnums.Gender;
+
+public class Coop {
+
+    public static void main(String[] args) {
+        FarmAnimal duck = new FarmAnimal(3, Gender.FEMALE);
+        Bird duckie = new Bird(4, Gender.MALE, "potaot");
+
+        System.out.println(duckie.eat());
+        System.out.println(duck.eat());
+    }
+}
