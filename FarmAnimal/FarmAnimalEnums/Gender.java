5e25afb2c2bcbce82c203458ad679fbdcfc4fcde
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-04-30 19:33:51 -0400

    Add files via upload

--- FarmAnimal/FarmAnimalEnums/Gender.java
+++ FarmAnimal/FarmAnimalEnums/Gender.java
@@ -1,4 +1,12 @@
-package FarmAnimalEnums;
+package farmanimalenums;
+
+/**
+* Gender enum with constant female or male values
+*
+* @author Kaili Tang
+* @author Agincourt CI
+* @version 6.0 Build 9000 - April 2025
+*/
 
 public enum Gender {
 
