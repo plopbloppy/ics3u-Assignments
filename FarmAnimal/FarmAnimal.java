a6a10b98b6f126afff01562376f7f95706dac6d6
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-05-01 07:48:17 -0400

    Update FarmAnimal.java

--- FarmAnimal/FarmAnimal.java
+++ FarmAnimal/FarmAnimal.java
@@ -10,18 +10,24 @@ import farmanimalenums.Gender;
 
 public class FarmAnimal {
 
+    /** the age of the farm animal in years */
     public int age;
+    /** the gender of the farm animal (male/female) */
     public Gender gender;
+    /** the name of the farm animal */
     public String name;
+    /** the amount of happiness the farm animal has, increases through interactions */
     public int happiness;
+    /** the amount of gifts the farm animal has given, dependent on happiness */
     public int numberOfGifts;
+    /** determines whether the farm animal has already eaten in the day or not */
     protected boolean isFull;
 
     /**
     * constructor that creates a farm animal with age and gender
     *
     * @param age age of farm animal
-    * @param gender gender of farm animal: male/female
+    * @param gender gender of farm animal (male/female)
     */
     public FarmAnimal(int age, Gender gender) {
         this.age = age;
@@ -42,86 +48,86 @@ public class FarmAnimal {
     }
 
     /**
-    * returns the age of farm animal
+    * returns the age of this farm animal
     *
-    * @return age of farm animal
+    * @return the age
     */
     public int getAge() {
         return age;
     }
 
     /**
-    * returns the gender of farm animal
+    * returns the gender of this farm animal
     *
-    * @return gender of farm animal
+    * @return the gender
     */
     public Gender getGender() {
         return gender;
     }
 
     /**
-    * returns the name of farm animal
+    * returns the name of this farm animal
     *
-    * @return name of farm animal
+    * @return the name
     */
     public String getName() {
         return name;
     }
 
     /**
-    * returns the happiness level of farm animal
+    * returns the happiness level of this farm animal
     *
-    * @return amount of happiness of farm animal
+    * @return the amount of happiness
     */
     public int getHappiness() {
         return happiness;
     }
 
     /**
-    * returns the number of gifts collected from farm animal
+    * returns the number of gifts collected from this farm animal
     *
-    * @return amount of gifts collected from farm animal
+    * @return the number of gifts
     */
     public int getNumberOfGifts() {
         return numberOfGifts;
     }
 
     /**
-    * sets age of farm animal
+    * sets the age of this farm animal
     *
-    * @param age new age of farm animal
+    * @param age the new age
     */
     public void setAge(int age) {
         this.age = age;
     }
 
     /**
-    * sets gender of farm animal
+    * sets the gender of this farm animal
     *
-    * @param gender new gender of farm animal
+    * @param gender the new gender
     */
     public void setGender(Gender gender) {
         this.gender = gender;
     }
 
     /**
-    * sets name of farm animal
+    * sets the name of this farm animal
     *
-    * @param name new name of farm animal
+    * @param name the new name
     */
     public void setName(String name) {
         this.name = name;
     }
 
-    //NEED TO STOP PPL FROM USING THE SAME NAME FOR ANIMAL
+    //NEED TO STOP POSSIBILITY OF SAME NAME FOR SAME ANIMAL
     protected boolean sameName(FarmAnimal animal) {
         return this.name.equalsIgnoreCase(animal.getName());
     }
 
     /**
-    * depending on amount of happiness, farm animal might give gift
+    * determines the type of gift this farm animal will give if happiness is in between 0 and 40
     *
-    * @returns type of gift if 0 < happiness < 40, otherwise null
+    * @returns the type of gift (true) or null (false)
     */
     protected String dropGift() {
         String gift;
@@ -140,8 +146,10 @@ public class FarmAnimal {
         return gift;
     }
 
-    /**
-    * if not full, farm animal eats and gains 2 happiness
+    /** 
+    * this farm animal eats and gains 2 happiness if it is not full
+    *
+    * @returns +2 happiness (true) or "Your animal is full!" (false)
     */
     public String eat() {
         if (isFull) {
@@ -154,8 +162,10 @@ public class FarmAnimal {
     }
 
     /**
-    * pet farm animal and gain 3 happiness
-    * farm animal will give gift if gift is not null, and number of gifts increases by 1
+    * pets this farm animal and gain 3 happiness
+    * this farm animal gives a gift if happiness is in between 0 and 40, and number of gifts increases by 1
+    *
+    * @return +3 happiness and gift (true) or null (false)
     */
     public String pet() {
         if (dropGift() != null) {
@@ -168,7 +178,10 @@ public class FarmAnimal {
     }
 
     /**
-    * farm animal will sleep, age 1 year, and gain 5 happiness
+    * this farm animal sleeps, ages 1 year, and gains 5 happiness
+    * sleeping allows the farm animal to eat again
+    *
+    * @return +5 happiness, +1 age
     */
     public String sleep() {
         age++;
